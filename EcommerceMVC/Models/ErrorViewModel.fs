7558d899183f31b3a8cf0cc02e21e30a7f5b4613
TienLoFi <ngoctiendz2k3@gmail.com>
2024-04-06 13:42:29 +0700

    update

---
+++ EcommerceMVC/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace EcommerceMVC.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
